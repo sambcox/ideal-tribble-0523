6938400e30b255478296d359aa29b326ebcf3c73
Samuel Cox <samc1253@gmail.com>
2023-01-02 10:49:44 -0700

    Add studio show view with all correct information

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/studios', to: 'studios#index'
+  get '/studios/:id', to: 'studios#show'
   get '/movies/:id', to: 'movies#show'
-  post 'movies/:movie_id/actors', to: 'actor_movies#create'
+  post '/movies/:movie_id/actors', to: 'actor_movies#create'
 end
