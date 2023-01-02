555885a6da30e150cb1c47336471c6e32b2b5eba
Samuel Cox <samc1253@gmail.com>
2023-01-02 10:11:40 -0700

    Add test for actors ordered by age

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -5,4 +5,33 @@ RSpec.describe Movie do
     it {should belong_to :studio}
     it {should have_many :actors }
   end
+
+  describe 'instance methods' do
+    before :each do
+      @universal = Studio.create!({name: 'Universal Studios', location: 'Hollywood'})
+      @paramount = Studio.create!({name: 'Paramount Pictures', location: 'Dana Point'})
+      @warner_brothers = Studio.create!({name: 'Warner Brothers Studios', location: 'Inglewood'})
+
+      @toy = @universal.movies.create!({title: 'Toy Story', creation_year: '1995', genre: 'animation'})
+      @monty = @universal.movies.create!({title: 'Monty Python and the Holy Grail', creation_year: '1972', genre: 'comedy'})
+      @airplane = @universal.movies.create!({title: 'Airplane!', creation_year: '1975', genre: 'comedy'})
+      @star = @paramount.movies.create!({title: 'Star Wars', creation_year: '1977', genre: 'sci-fi'})
+      @italian = @paramount.movies.create!({title: 'The Italian Job', creation_year: '1968', genre: 'crime'})
+
+      @meryl = @toy.actors.create!({name: 'Meryl Streep', age: 73})
+      @tom = @toy.actors.create!({name: 'Tom Hanks', age: 65})
+      @michael = @italian.actors.create!({name: 'Michael Caine', age: 90})
+      @john = @monty.actors.create!({name: 'John Cleese', age: 82})
+      @mark = @star.actors.create!({name: 'Mark Hamill', age: 75})
+      @harrison = @star.actors.create!({name: 'Harrison Ford', age: 80})
+      @leslie = @airplane.actors.create!({name: 'Leslie Nielsen', age: 84})
+
+      ActorMovie.create!({actor_id: @leslie.id, movie_id: @monty.id})
+      ActorMovie.create!({actor_id: @harrison.id, movie_id: @italian.id})
+    end
+
+    it 'returns all actors for a movie ordered by age' do
+      expect(@toy.actors_by_age).to eq([@tom, @meryl])
+    end
+  end
 end
